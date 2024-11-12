32e16cbf71fa1dfd8df3ce5ef69e707f094b3682
Simon Bard <simon.bard4@gutenbergcerts.com>
2024-11-12 16:00:16 -0500

    fix next config

--- next.config.ts
+++ next.config.ts
@@ -3,12 +3,12 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   reactStrictMode: true,
-  webpackDevMiddleware: config => {
+  webpackDevMiddleware: (config: any) => {
     config.watchOptions = {
       poll: 1000,
       aggregateTimeout: 300,
-    }
-    return config
+    };
+    return config;
   },
 };
 
