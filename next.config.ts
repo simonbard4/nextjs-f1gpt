e1abc68214db52ea0059d00d2b1f1bab5ecfb08c
Simon Bard <simon.bard4@gutenbergcerts.com>
2024-11-12 15:45:17 -0500

    enable hot reloading docker

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,13 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   reactStrictMode: true,
+  webpackDevMiddleware: config => {
+    config.watchOptions = {
+      poll: 1000,
+      aggregateTimeout: 300,
+    }
+    return config
+  },
 };
 
 export default nextConfig;
