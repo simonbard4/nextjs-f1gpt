0979731653f4ef0cb32cb000c638c5b034a9c5d7
Simon Bard <simon.bard4@gutenbergcerts.com>
2024-11-13 17:15:54 -0500

    initialize open ai / f1 data

--- scripts/loadDb.ts
+++ scripts/loadDb.ts
@@ -1,7 +1,6 @@
 import { DataAPIClient } from "@datastax/astra-db-ts"
 import { PuppeteerWebBaseLoader } from "langchain/document_loaders/web/puppeteer"
 import OpenAI from "openai"
-
 import { RecursiveCharacterTextSplitter } from "langchain/text_splitter"
 
 import "dotenv/config"
@@ -13,3 +12,19 @@ const {
     ASTRA_DB_APPLICATION_TOKEN,
     OPENAI_API_KEY
 } = process.env
+
+const openai = new OpenAI({ apiKey: OPENAI_API_KEY })
+
+const f1Data = [
+    'https://en.wikipedia.org/wiki/Formula_One',
+    'https://www.skysports.com/f1/news/12433/13117256/lewis-hamilton-says-move-to-ferrari-from-mercedes-doesnt-need-vindicating-amid-irritation-at-coverage',
+    'https://www.formula1.com/en/latest/all',
+    'https://www.forbes.com/sites/brettknight/2023/11/29/formula-1s-highest-paid-drivers-2023/',
+    'https://www.autosport.com/f1/news/history-of-female-f1-drivers-including-grand-prix-starters-and-test-drivers/10584871/',
+    'https://en.wikipedia.org/wiki/2023_Formula_One_World_Championship',
+    'https://en.wikipedia.org/wiki/2022_Formula_One_World_Championship',
+    'https://en.wikipedia.org/wiki/List_of_Formula_One_World_Drivers%27_Champions',
+    'https://en.wikipedia.org/wiki/2024_Formula_One_World_Championship',
+    'https://www.formula1.com/en/results.html/2024/races.html',
+    'https://www.formula1.com/en/racing/2024.html'
+]
