581b5f775fcaaceba41b3e61636197c11197c808
Simon Bard <simon.bard4@gutenbergcerts.com>
2024-11-13 17:05:01 -0500

    setup dotenv config

---
+++ scripts/loadDb.ts
@@ -0,0 +1,15 @@
+import { DataAPIClient } from "@datastax/astra-db-ts"
+import { PuppeteerWebBaseLoader } from "langchain/document_loaders/web/puppeteer"
+import OpenAI from "openai"
+
+import { RecursiveCharacterTextSplitter } from "langchain/text_splitter"
+
+import "dotenv/config"
+
+const {
+    ASTRA_DB_NAMESPACE,
+    ASTRA_DB_COLLECTION,
+    ASTRA_DB_API_ENDPOINT,
+    ASTRA_DB_APPLICATION_TOKEN,
+    OPENAI_API_KEY
+} = process.env
