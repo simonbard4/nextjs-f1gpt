12717e579a3a3df759aef7f2a015ffb8480e0cd3
Simon Bard <simon.bard4@gutenbergcerts.com>
2024-11-14 22:12:59 -0500

    scrape and load data

--- scripts/loadDb.ts
+++ scripts/loadDb.ts
@@ -48,3 +48,45 @@ const createCollection = async (similarityMetric: SimilarityMetric = "dot_produc
     })
     console.log(res)
 }
+
+const loadSampleData = async () => {
+    const collection = await db.collection(ASTRA_DB_COLLECTION)
+    for await ( const url of f1Data ) {
+        const content = await scrapePage(url)
+        const chunks = await splitter.splitText(content)
+        for await ( const chunk of chunks ) {
+            const embedding = await openai.embeddings.create({
+                model: "text-embedding-3-small",
+                input: chunk,
+                encoding_format: "float"
+            })
+
+            const vector = embedding.data[0].embedding
+
+            const res = await collection.insertOne({
+                $vector: vector,
+                text: chunk
+            })
+            console.log(res)
+        }
+    }
+}
+
+const scrapePage = async (url: string) => {
+    const loader = new PuppeteerWebBaseLoader(url, {
+        launchOptions: {
+            headless: true
+        },
+        gotoOptions: {
+            waitUntil: "domcontentloaded"
+        },
+        evaluate: async (page, browser) => {
+            const result = await page.evaluate(() => document.body.innerHTML)
+            await browser.close()
+            return result
+        }
+    })
+    return ( await loader.scrape())?.replace(/<[^>]*>?/gm, '')
+}
+
+createCollection().then(() => loadSampleData())
